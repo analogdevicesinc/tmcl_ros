d588718549cde69647da341da319b670816e63ca
mmaralit-adi <Maggie.Maralit@analog.com>
2023-09-26 16:48:08 +0800

    Remove obsolete modules, update copyrights' years, update package.xml version and cleanup CMakeLists.txt
    - Remove obsolete modules
    - Update copyrights' years
    - Update package.xml version
    - Clean-up comments in CMakeLists.txt
    
    Co-Authored-By: Christian Joseph Acar <124771470+CAcarADI@users.noreply.github.com>

--- src/socket_can_wrapper.cpp
+++ src/socket_can_wrapper.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Analog Devices, Inc. All Rights Reserved.
+ * Copyright (c) 2022-2023 Analog Devices, Inc. All Rights Reserved.
  * This software is proprietary to Analog Devices, Inc. and its licensors.
  **/
 
